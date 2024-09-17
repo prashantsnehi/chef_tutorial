8b814dd199b950bb16c5bf877f368ca93872ee5b
prashantsnehi <prashant.snehi@gmail.com>
2024-09-18 00:36:24 +0530

    added comments in the test-recipe-2.rb file

--- recipes/test-recipe-2.rb
+++ recipes/test-recipe-2.rb
@@ -4,15 +4,20 @@
 #
 # Copyright:: 2024, The Authors, All Rights Reserved.
 
+# installing Apache on Redhat Linux and creating index file in the default path
+
+# installing Apache
 package 'httpd' do
   action :install
 end
 
+# Creating file in the default path
 file '/var/www/html/index.html' do
   content 'Welcome to world of Chef'
   action :create
 end
 
+# Enabling service on startup and starting httpd service
 service 'httpd' do
   action [:enable, :start]
 end
